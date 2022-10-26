cacc4cb30f08c1ef12acb8ff5d1d7c69b7660ed4
Maia A. Doxtader <maiadoxtader@proton.me>
2022-10-26 18:16:06 -0400

    spheres

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,60 @@
 
-use std::io::{stderr, Write};
 mod math;
-use math::{Float3, ColorRGB};
+use math::{Float3, ColorRGB, Ray, Point3};
+
+// ray-tracer following the Ray Tracing in a Weekend book in Rust
+use std::io::{stderr, Write};
+
+
+
+fn ray_color(r: &Ray) -> ColorRGB {
+
+   let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
+
+    if t > 0.0 {
+        let n = (r.at(t) - Float3::new(0.0, 0.0, -1.0)).normalized(); // normalized surface normal
+        return 0.5 * ColorRGB::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
+    }
+
+    let unit_direction = r.direction().normalized();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * ColorRGB::new(1.0, 1.0, 1.0) + t * ColorRGB::new(0.5, 0.7, 1.0)
+
+}
+
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
+    let oc = r.origin() - center;
+    let a = r.direction().length().powi(2);
+    let half_b = oc.dot(r.direction());
+    let c = oc.length().powi(2) - radius * radius;
+    let discriminant = half_b * half_b - a * c;
+
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-half_b - discriminant.sqrt()) /  a
+    }
+
+}
+
+
 fn main() {
+    
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: u64 = 256;
-    const IMAGE_HEIGHT: u64 = 256;
+    const IMAGE_HEIGHT: u64 = ((256 as f64) / ASPECT_RATIO) as u64;
+
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Float3::new(viewport_width, 0.0, 0.0);
+    let vertical = Float3::new(0.0, viewport_height, 0.0);
+
+    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 
+                                - Float3::new(0.0, 0.0, focal_length);
+
 
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
@@ -16,15 +66,14 @@ fn main() {
 
 
         for i in 0..IMAGE_WIDTH {
-            let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
-            let g = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
-            let b = 0.25;
+            let u = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let v = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
 
-            let ir = (255.999 * r) as u64;
-            let ig = (255.999 * g) as u64;
-            let ib = (255.999 * b) as u64;
-            
-            println!("{} {} {}", ir, ig, ib);
+            let r = Ray::new(origin, 
+                            lower_left_corner + u * horizontal + v * vertical - origin);
+            let pixel_color = ray_color(&r);
+
+            println!("{}", pixel_color.format_color());
         }
     }
     eprintln!("\nDone.");
--- src/math.rs
+++ src/math.rs
@@ -1,5 +1,8 @@
 use std::ops::{Index, IndexMut, Add, AddAssign, Sub, SubAssign, Mul, MulAssign, Div, DivAssign};
 
+use std::fmt;
+use std::fmt::Display;
+
 #[derive(Clone, Copy)]
 pub struct Float3 {
     data: [f64; 3]
@@ -30,3 +33,172 @@ impl IndexMut<usize> for Float3 {
     }
 }
 
+impl Add for Float3 {
+    type Output = Float3;
+
+    fn add(self, other: Float3) -> Float3 {
+        Float3 {
+            data: [self[0] + other[0], self[1] + other[1], self[2] + other[2]]
+        }
+    }
+}
+
+impl AddAssign for Float3 {
+    fn add_assign(&mut self, other: Float3) -> () {
+        *self = Float3 {
+            data:[ self[0] + other[0], self[1] + other[1], self[2] + other[2]]
+        };
+    }
+}
+
+impl Sub for Float3 {
+    type Output = Float3;
+    
+    fn sub(self, other: Float3) -> Float3 {
+        Float3 {
+            data: [self[0] - other[0], self[1] - other[1], self[2] - other[2]]
+        }
+    }
+}
+
+impl SubAssign for Float3 {
+    fn sub_assign(&mut self, other: Float3) -> () {
+        *self = Float3 {
+            data: [self[0] - other[0], self[1] - other[1], self[2] - other[2]]
+        };
+    }
+}
+
+impl Mul<f64> for Float3 {
+    type Output = Float3;
+
+    fn mul(self, other: f64) -> Float3 {
+        Float3 {
+            data: [self[0] * other, self[1] * other, self[2] * other]
+        }
+    }
+}
+
+impl MulAssign<f64> for Float3 {
+    fn mul_assign(&mut self, other: f64) -> () {
+        *self = Float3 {
+            data: [self[0] * other, self[1] * other, self[2] * other]
+        };
+    }
+}
+
+impl Mul<Float3> for f64 {
+    type Output = Float3;
+
+    fn mul(self, other:Float3) -> Float3 {
+        Float3 {
+            data: [self * other[0], self * other[1], self * other[2]]
+        }
+    }
+}
+
+impl Div<f64> for Float3 {
+    type Output = Float3;
+
+    fn div(self, other:f64) -> Float3 {
+        Float3 {
+            data: [self[0] / other, self[1] / other, self[2] / other]
+        }
+    }
+}
+
+impl DivAssign<f64> for Float3 {
+    fn div_assign(&mut self, other:f64) -> () {
+        *self = Float3 {
+            data: [self[0] / other, self[1] / other, self[2] / other]
+        };
+    }
+}
+
+impl Float3 {
+
+    pub fn x(self) -> f64 {
+        self[0]
+    }
+
+    pub fn y(self) -> f64 {
+        self[1]
+    }
+
+    pub fn z(self) -> f64 {
+        self[2]
+    }
+
+    pub fn dot(self, other: Float3) -> f64 {
+        self[0] * other[0] + self[1] * other[1] + self[2] * other[2]
+    }
+
+    pub fn length(self) -> f64 {
+        self.dot(self).sqrt()
+    }
+
+    pub fn cross(self, other: Float3) -> Float3 {
+        Float3 {
+            data: [
+                self[1] * other[2] - self[2] * other[1],
+                self[2] * other[0] - self[0] * other[2],
+                self[0] * other[1] - self[1] * other[0]
+            ]
+        }
+    }    
+
+    pub fn normalized(self) -> Float3 {
+        self / self.length()
+    }
+
+    pub fn format_color(self) -> String {
+        format!("{} {} {}", (255.999 * self[0]) as u64,
+                            (255.999 * self[1]) as u64,
+                            (255.999 * self[2]) as u64)
+    }
+}
+
+impl Display for Float3 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {}, {})", self[0], self[1], self[2])
+    }
+}
+
+
+// ------------------------------------------------------------------------
+
+pub struct Ray {
+    orig: Point3, 
+    dir: Float3
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Float3) -> Ray {
+        Ray {
+            orig: origin,
+            dir: direction
+        }
+    }
+
+    pub fn origin(&self) -> Point3 {
+        self.orig
+    }
+
+    pub fn direction(&self) -> Float3 {
+        self.dir
+    }
+
+    pub fn at(&self, t:f64) -> Point3 {
+        self.orig + t * self.dir
+    }
+}
+
+pub struct HitRecord {
+    p: Point3,
+    normal: Float3,
+    t: f64
+}
+
+pub trait Hit {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
+}
